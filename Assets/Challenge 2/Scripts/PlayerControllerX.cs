d9fb7a0b4b111a18586233b4b1039687edd35ef9
alex-24-cyber <146207916+alex-24-cyber@users.noreply.github.com>
2023-10-13 18:34:15 +0100

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 1; // initialize to a positive value
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1; // reset the cooldown
+        }
+        
+        // update the cooldown every frame
+        if (cooldown >= 0) {
+            cooldown -= Time.deltaTime; 
+        }
+
+    }
+}
+
